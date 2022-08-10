8dfac8dd05876b9f7e6d51957b4e8ce7a3566f0b
Sreekar Reddy Aparacherla <sreekar.reddy.aparacherla@gmail.com>
2022-08-10 14:37:35 +0800

    Create a basic navbar and remove redundant create-react-app overheads

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+import { strings } from '../resources/strings';
+
+export const Navbar = () => {
+    const StickyTopDiv = styled.div`
+        position: -webkit-sticky; /* Safari */
+        position: sticky;
+        top: 0;
+        margin: 0;
+        background-color: black;
+    `;
+
+    const EmphaticHeader = styled.h2`
+        padding: 1%;
+        font-family: monospace;
+        color: white;
+    `;
+
+
+    return (
+        <StickyTopDiv>
+            <EmphaticHeader>{strings.navbarTitle}</EmphaticHeader>
+        </StickyTopDiv>
+    )
+}
